d3d44653ea3c041628b22d33944c4d1e9750a49a
Mitchell <Hmitchell.hall@gmail.com>
2021-08-24 09:48:36 -0500

    all done except like button

--- js/challenge.js
+++ js/challenge.js
@@ -4,28 +4,50 @@ const plusButton = document.querySelector("#plus")
 const minusButton = document.querySelector("#minus")
 const heartButton = document.querySelector("#heart")
 const submitButton = document.querySelector("#submit")
-const pausedButtons = () => {plusButton, minusButton, heartButton, submitButton}
+let timer ;
+
 
 function timeoutHandler() {
   updateTimer();
   if (num < 60) {
     num++;
- var timer = setTimeout(timeoutHandler, 1000);
+ timer = setTimeout(timeoutHandler, 1000);
   }
-  pauseButton.addEventListener('click', event => {clearTimeout(timer)
-  pauseButton.textContent = 'resume'
-  pauseButton.setAttribute('id', 'paused' )
-})
 }
 function updateTimer() {
   document.querySelector("#counter").textContent = num;
 }
 timeoutHandler();
 
-pauseButton.addEventListener('click', () => plusButton.setAttribute('disabled', 'true' ))
-pauseButton.addEventListener('click', () => minusButton.setAttribute('disabled', 'true'))
-pauseButton.addEventListener('click', () => heartButton.setAttribute('disabled', 'true'))
-pauseButton.addEventListener('click', () => submitButton.setAttribute('disabled', 'true'))
-plusButton.addEventListener('click', () => document.querySelector("#counter").textContent= num+=1 )
-minusButton.addEventListener('click', () => document.querySelector("#counter").textContent= num-=1 )
+pauseButton.addEventListener('click', event => {clearTimeout(timer)
+    pauseButton.textContent = 'resume'
+  if(pauseButton.id === 'paused'){
+      //console.log('resuming')
+      timeoutHandler()
+    pauseButton.setAttribute('id', 'pause')
+    plusButton.removeAttribute('disabled')
+    minusButton.removeAttribute('disabled')
+    heartButton.removeAttribute('disabled')
+    submitButton.removeAttribute('disabled')
+}
+else{
+    //console.log('pausing')
+  pauseButton.setAttribute('id', 'paused' )
+  plusButton.setAttribute('disabled', 'true')
+  minusButton.setAttribute('disabled', 'true')
+  heartButton.setAttribute('disabled', 'true')
+  submitButton.setAttribute('disabled', 'true')
+}
+})
+plusButton.addEventListener('click', () => document.querySelector("#counter").textContent= num+=1)
+minusButton.addEventListener('click', () => document.querySelector("#counter").textContent= num-=1)
 
+document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault()
+    handleSubmit(e.target.comment.value)
+})
+function handleSubmit(comments){
+    let p = document.createElement('p')
+    p.textContent = comments
+    document.querySelector("#list").appendChild(p)
+}
